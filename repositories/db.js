be9bedf7aa25a47221a84fdb9c0579d91b982a83
Marcelo Fazan <56393266+Marcelofazan@users.noreply.github.com>
2026-06-12 14:15:13 -0300

    Clean up db.js by removing console log
    Removed unnecessary console log and extra line breaks.

--- repositories/db.js
+++ repositories/db.js
@@ -4,9 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config'
 
 const supabase = createClient(process.env.SUPABASE_DB_URL, process.env.SUPABASE_SERVICE_KEY);
-
 console.log(process.env);
-
 
 const sequelize = new Sequelize('postgresql://postgres:db_s3rv3r_123@db.ncdiywtvargeyjrbegfi.supabase.co:5432/postgres', {
   dialect: 'postgres',
@@ -18,9 +16,7 @@ const sequelize = new Sequelize('postgresql://postgres:db_s3rv3r_123@db.ncdiywtv
   }
 });
 
-
 export default { sequelize, supabase };
-
 
 // Test connection
 async function testConnection() {
